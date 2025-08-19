a0db7ed1bec08508bf0a93cacd7b04ff6f252d41
Sunil Kumar <sunil.kumar@crowdstrike.com>
2025-08-19 16:39:32 +0200

    more control with env

--- log_init/src/lib.rs
+++ log_init/src/lib.rs
@@ -1,30 +1,49 @@
-use std::io::stdout;
+use std::{env, io::stdout, str::FromStr};
 
+use chrono::Local;
 use fern::log_file;
 use log::debug;
 
 pub fn init_logging(){
-    let file_name = "prof_info.log";
-    match log_file(file_name) {
+
+    let log_level = match env::var_os("RUST_LOG"){
+        Some(val) => log::LevelFilter::from_str(
+            val.into_string()
+                .unwrap_or(String::from("info"))
+                .as_str()
+            )
+            .unwrap_or(log::LevelFilter::Info),
+        None => log::LevelFilter::Info,
+    };
+
+    let log_file_path = env::var_os("LOG_FILE")
+        .unwrap_or("prof_info.log".into())
+        .to_string_lossy()
+        .to_string();
+
+    match log_file(log_file_path.as_str()) {
         Ok(file_dispatch) =>
             match fern::Dispatch::new()
             .format(|out, message, record| {
-                out.finish(format_args!("{} [{}]-[{}#{}] {}",
+                out.finish(format_args!("{} [{}] {}",
                     record.level(),
                     record.target(),
-                    record.file().unwrap_or("-"),
-                    record.line().unwrap_or(0),
+                    //record.file().unwrap_or("-"),
+                    //record.line().unwrap_or(0),
                     message
                 ));
             })
-            .level(log::LevelFilter::Debug)
+            .level(log_level)
             .chain(stdout())
             .chain(file_dispatch)
             .apply() 
             {
-                Ok(_) => debug!("logging initialized"),
+                Ok(_) => {
+                    debug!("---------- starting new run at {:?} -----------", Local::now());
+                    debug!("logging initialized with level {log_level}")
+                },
                 Err(err) => eprintln!("init_logging failed with {err}"),
             },
-        Err(err) => eprintln!("failed to create log file '{file_name}'; err {err}")
+        Err(err) => eprintln!("failed to create log file '{log_file_path}'; err {err}")
     }
 }
